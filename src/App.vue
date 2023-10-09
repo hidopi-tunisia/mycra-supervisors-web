5e4f42016e3a60a127b86bb5cbde5f672ae5adfd
Sofienne LASSOUED <soflass1293@gmail.com>
2023-10-09 16:53:03 +0100

    Added layout

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,11 @@
 <template>
-  <router-view></router-view>
+  <app-layout />
+  <app-footer />
 </template>
 
-<script setup></script>
+<script setup lang="ts">
+import AppLayout from '@/layout/app-layout.vue'
+import AppFooter from '@/components/shared/app-footer.vue'
+</script>
 
 <style lang="scss" scoped></style>
---
+++ src/layout/app-layout.vue
@@ -0,0 +1,30 @@
+<template>
+  <!-- Layout wrapper -->
+  <div class="layout-wrapper layout-content-navbar">
+    <div class="layout-container">
+      <app-menu />
+      <div class="layout-page">
+        <app-navbar />
+        <div class="content-wrapper">
+          <div class="container-xxl flex-grow-1 container-p-y">
+            <router-view></router-view>
+          </div>
+
+          <div class="content-backdrop fade"></div>
+        </div>
+      </div>
+    </div>
+
+    <!-- Overlay -->
+    <div class="layout-overlay layout-menu-toggle"></div>
+  </div>
+  <!-- / Layout wrapper -->
+</template>
+
+<script setup lang="ts">
+import AppNavbar from '@/components/shared/app-navbar.vue'
+import AppMenu from '@/components/shared/app-menu.vue'
+
+</script>
+
+<style scoped></style>
