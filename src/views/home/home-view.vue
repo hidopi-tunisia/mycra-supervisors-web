f8c94e1cae004eb9ab21023c17468c923f023c16
Sofienne LASSOUED <soflass1293@gmail.com>
2023-10-12 19:38:12 +0100

    Changed to absences chart

--- src/views/home/home-view.vue
+++ src/views/home/home-view.vue
@@ -98,12 +98,12 @@
         </div>
       </div>
     </div>
-    <!-- Total Revenue -->
+    <!-- Absences -->
     <div class="col-12 col-lg-8 order-2 order-md-3 order-lg-2 mb-4">
       <div class="card">
         <div class="row row-bordered g-0">
           <div class="col-md-12">
-            <h5 class="card-header m-0 me-2 pb-3">CRAs saisis chaques années</h5>
+            <h5 class="card-header m-0 me-2 pb-3">Taux d'absence chaques années</h5>
             <div id="chart-consultants-absence" class="px-2"></div>
           </div>
         </div>
