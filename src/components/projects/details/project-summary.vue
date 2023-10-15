9220887768581e0fa4776a6191dc28b52bcafb05
Sofienne LASSOUED <soflass1293@gmail.com>
2023-10-15 19:06:12 +0100

    Fixed project picker sizing

--- src/components/projects/details/project-summary.vue
+++ src/components/projects/details/project-summary.vue
@@ -1,20 +1,21 @@
 <template>
   <div class="row">
+    <div class="mt-4 d-md-none"></div>
     <div class="col-12">
       <div class="row">
-        <div class="col-4">
+        <div class="col-xs-12 col-sm-6 col-md-4">
           <div class="mb-2">Nom</div>
           <div class="fw-bold">
             {{ props.data.name }}
           </div>
         </div>
-        <div class="col-4">
+        <div class="col-xs-12 col-sm-6 col-md-4">
           <div>Catégorie</div>
           <div class="fw-bold my-2">
             {{ props.data.category }}
           </div>
         </div>
-        <div class="col-4">
+        <div class="col-xs-12 col-sm-6 col-md-4">
           <div>Statut</div>
           <span class="badge bg-label-success me-1" v-show="props.data!.status === 'active'">
             Active
@@ -25,13 +26,13 @@
         </div>
       </div>
       <div class="row my-2">
-        <div class="col-4">
+        <div class="col-xs-12 col-sm-6 col-md-4">
           <div class="mb-2">Code</div>
           <div class="fw-bold my-2 text-uppercase">
             {{ props.data.projectCode.substring(0, 8) }}
           </div>
         </div>
-        <div class="col-4">
+        <div class="col-xs-12 col-sm-6 col-md-4">
           <div class="mb-2">Client</div>
           <div class="fw-bold my-2 text-uppercase">
             {{ props.data.client }}
@@ -57,19 +58,19 @@
         </div>
       </div>
       <div class="row my-2">
-        <div class="col-4">
+        <div class="col-xs-12 col-sm-6 col-md-4">
           <div>Date de début</div>
           <div class="fw-bold my-2">
             {{ props.data.startDate.substring(0, 10) }}
           </div>
         </div>
-        <div class="col-4">
+        <div class="col-xs-12 col-sm-6 col-md-4">
           <div>Date de fin</div>
           <div class="fw-bold my-2">
             {{ props.data.endDate.substring(0, 10) }}
           </div>
         </div>
-        <div class="col-4">
+        <div class="col-xs-12 col-sm-6 col-md-4">
           <div>Date de création</div>
           <div class="fw-bold my-2">
             {{ props.data.createdAt.substring(0, 10) }}
