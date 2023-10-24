0b7efcb95107ccdab4d6982b53aba6df8511d835
Sofienne LASSOUED <soflass1293@gmail.com>
2023-10-24 17:34:01 +0100

    Added notification preview

---
+++ src/components/notifications/notification-preview.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="wrapper">
+    <div class="mockup-header">
+      <img
+        src="https://static.vecteezy.com/system/resources/previews/026/590/927/non_2x/cra-logo-design-inspiration-for-a-unique-identity-modern-elegance-and-creative-design-watermark-your-success-with-the-striking-this-logo-vector.jpg"
+        class="mockup-app-logo"
+      />
+      <div class="mx-1"></div>
+      <div class="mockup-app-name">My CRA</div>
+    </div>
+
+    <div class="mockup-content">
+      <p class="mockup-content-text">{{ props.content }}</p>
+    </div>
+    <img src="assets/img/mockups/iphone.png" class="mockup" />
+  </div>
+</template>
+
+<script setup lang="ts">
+const props = defineProps(["content"])
+</script>
+<style scoped>
+@import './styles.css';
+</style>
