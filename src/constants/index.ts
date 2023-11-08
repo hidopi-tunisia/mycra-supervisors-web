2fa1796813589e41f903cd559a123fddb21f01b0
sofienne.lassoued <sofienne.lassoued@hidopi.com>
2023-11-08 18:31:08 +0100

    Reverted to hosted endpoint

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,2 +1,3 @@
-export const ENDPOINT = "http://localhost:30001"
+export const ENDPOINT = "https://apidevmycra.hidopi.com"
+// export const ENDPOINT = "http://localhost:30001"
 // export const ENDPOINT = "http://164.90.228.194:30001"
